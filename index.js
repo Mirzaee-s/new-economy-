011e9607dade38321473e9b90985565b21de410d
Mirzaee-s <mirzaee.s@gmail.com>
2025-02-02 23:32:56 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("پلتفرم اقتصادی جدید آنلاین شد!");
+    res.send("پلتفرم اقتصادی جدید آنلاین شد!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// نمایش صفحه ثبت‌نام
-app.get("/register", (req, res) => {
-    res.sendFile(__dirname + "/register.html");
-});
-
-// دریافت اطلاعات و ثبت در دیتابیس
-app.post("/register", (req, res) => {
-    const { name, phone, password } = req.body;
-    
-    // ذخیره در دیتابیس (در آینده اضافه می‌شود)
-    console.log(`کاربر جدید: ${name} - ${phone}`);
-    
-    res.send("ثبت‌نام با موفقیت انجام شد!");
-});
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
-console.log("برنامه در حال اجرا است...");
